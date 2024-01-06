1803df6219d760688cea003438c85b0cb5826fda
alaasao <a_souchi@estin.dz>
2024-01-06 20:34:27 +0100

    first section

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,23 @@ import React , {useState} from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import 'react-toastify/dist/ReactToastify.css';
 import { ToastContainer } from "react-toastify";
+import NavBar from "./utils/NavBar";
+import FirstSec from "./utils/FirstSec";
+import Context from "./Context";
 function App() {
   const [modal, setModal] = useState(false)
   const [typeStart, setTypeStart] = useState('')
   const [typeEnd, setTypeEnd] = useState('')
   const toogleModal = () => {
     setModal(!modal)
+    console.log(modal)
   }
   return (
-    <div>
-        NexZero Template
-    </div>
+    <Context.Provider value={{ toogleModal, modal }}>
+      <div className="w-[100%] bg-[#1B1919]">
+      <NavBar />
+      <FirstSec /></div>
+    </Context.Provider>
   );
 }
 
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,11 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'good-times': ['Good Times Rg', 'sans-serif'],
+      }
+    },
   },
   plugins: [],
 }
