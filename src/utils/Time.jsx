8e1d9a1cfc945219326a2aa43bd6f4b18b34e6c8
aceiny <a_zeraibi@estin.dz>
2024-01-13 15:26:09 +0100

    ??

--- src/utils/Time.jsx
+++ src/utils/Time.jsx
@@ -33,7 +33,7 @@ const Time = () => {
         <section className="section h-[100vh] bg-[#1B1919] mx-[auto] my-[auto] flex flex-col justify-center items-center">
             <h1 className="text-[#F05323] text-center text-[60px] font-[700] font-good-times mb-[10px] uppercase [text-shadow:0px_0px_17.8px_#F05323]">Time Left</h1>
             <div className="relative">
-                <img src={laptop} className=""  alt="" />
+                <img src={laptop} className="  "  alt="" />
                 <div className="gap-3 rounded-[12px] bg-[rgba(0,_1,_1,_0.63)] backdrop-filter backdrop-blur-[2.549999952316284px] absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] flex justify-between text-center px-[10px] py-[20px]">
                     <div className="w-[80px] flex flex-col">
                         <span className="text-[#FFF] font-[Poppins] leading-[30px] font-[500] uppercase	text-center	">Days</span>
