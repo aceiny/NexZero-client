297bcb2246e18d86958394e8e649613e99753264
Rayane-Boucheraine <rayaneha08@gmail.com>
2024-01-13 15:48:19 +0100

    laptop picture

--- src/utils/Time.jsx
+++ src/utils/Time.jsx
@@ -1,8 +1,5 @@
 import React from "react";
 import { useState, useEffect } from "react";
-import screen from '../../public/assets/screen.png'
-import blackbg from '../../public/assets/black.png'
-import binaire from '../../public/assets/binaire.png'
 import laptop from '../../public/assets/laptop.png'
 const Time = () => {
     const eventDate = new Date("2024-01-26T18:00:00");
@@ -31,9 +28,9 @@ const Time = () => {
 
     return(
         <section className="section h-[100vh] bg-[#1B1919] mx-[auto] my-[auto] flex flex-col justify-center items-center">
-            <h1 className="text-[#F05323] text-center text-[60px] font-[700] font-good-times mt-[10%] uppercase [text-shadow:0px_0px_17.8px_#F05323]">Time Left</h1>
+            <h1 className="text-[#F05323] text-center text-[60px] font-[700] font-good-times mt-[4%] uppercase [text-shadow:0px_0px_17.8px_#F05323]">Time Left</h1>
             <div className="relative">
-                <img src={laptop} className=" "  alt="" />
+                <img src={laptop} className="w-[900px] "  alt="" />
                 <div className="gap-3 rounded-[12px] bg-[rgba(0,_1,_1,_0.63)] backdrop-filter backdrop-blur-[2.549999952316284px] absolute top-[50%] translate-y-[-50%] left-[50%] translate-x-[-50%] flex justify-between text-center px-[10px] py-[20px]">
                     <div className="w-[80px] flex flex-col">
                         <span className="text-[#FFF] font-[Poppins] leading-[30px] font-[500] uppercase	text-center	">Days</span>
