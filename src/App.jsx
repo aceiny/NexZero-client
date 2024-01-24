666fb8a9a84d6669e3cfb85cfd0a2f1a1469838a
alaasao <a_souchi@estin.dz>
2024-01-24 21:47:24 +0100

    reg

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,31 @@
-import React , {useState} from "react";
+import React, { useState } from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
-import 'react-toastify/dist/ReactToastify.css';
+import "react-toastify/dist/ReactToastify.css";
 import { ToastContainer } from "react-toastify";
 import NavBar from "./utils/NavBar";
 import FirstSec from "./utils/FirstSec";
-import Time from "./utils/Time"
-import About from "./utils/About"
+import Time from "./utils/Time";
+import About from "./utils/About";
 import Context from "./Context";
+import Reg from "./utils/Reg";
+
 function App() {
-  const [modal, setModal] = useState(false)
-  const [typeStart, setTypeStart] = useState('')
-  const [typeEnd, setTypeEnd] = useState('')
+  const [modal, setModal] = useState(false);
+  const [typeStart, setTypeStart] = useState("");
+  const [typeEnd, setTypeEnd] = useState("");
   const toogleModal = () => {
-    setModal(!modal)
-    console.log(modal)
-  }
+    setModal(!modal);
+    console.log(modal);
+  };
   return (
     <Context.Provider value={{ toogleModal, modal }}>
       <div className="w-[100%] bg-[#1B1919]">
         <NavBar />
-        <div className="container_wrapper">
+        <div
+          className="container_wrapper"
+          onClick={() => setModal((prev) => (prev === true ? false : prev))}
+        >
+          <Reg />
           <FirstSec />
           <Time />
           <About />
@@ -29,4 +35,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
