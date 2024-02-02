bcc9d1705cad1a961032addb8652693119581b98
Yasseur Boutobba <y_boutobba@estin.dz>
2024-02-02 01:08:53 +0100

    last section part 2

--- src/utils/MentorCard.jsx
+++ src/utils/MentorCard.jsx
@@ -1,7 +1,17 @@
 const MentorCard = ({ mentor }) => {
   return (
-    <div>
-      <h1>heyy</h1>
+    <div className=" flex flex-col md:flex-row gap-8 items-center p-8 bg-white/5 rounded-3xl backdrop-blur-sm">
+      <div className=" p-4 rounded-[2rem] shadow-[0px_0px_16px_#F05323]">
+        <img className=" rounded-[2.2rem] w-[250px] h-[250px]" src={mentor.image} alt="" />
+
+      </div>
+      <div className=" flex flex-col gap-2 md:gap-4 text-xl md:text-2xl font-bold text-gray-50 capitalize [text-shadow:0px_0px_2px_#fff] ">
+        <h1>name : <span className=" font-medium ml-4">{mentor.name}</span> </h1>
+        <h1>surname : <span className=" font-medium ml-4">{mentor.surname}</span></h1>
+        <h1>job : <span className=" font-medium ml-4">{mentor.job}</span></h1>
+        <h1>experience : <span className=" font-medium ml-4">{mentor.experience}</span></h1>
+      </div>
+      
     </div>
   );
 };
