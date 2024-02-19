74ded638853e8871da18f3da3e9c3a7ea1ca76b0
alaasao <a_souchi@estin.dz>
2024-02-19 10:51:16 +0100

    faq final

--- src/utils/Faq.jsx
+++ src/utils/Faq.jsx
@@ -1,62 +1,11 @@
-// import data from "./faqData";
-// import { useState } from "react";
-// const Faq = () => {
-//   let [list, setList] = useState(data);
-//   function handle(id) {
-//     console.log(id);
-//     setList(
-//       list.map((e) => {
-//         if (e.id === id) {
-//           return { ...e, state: "side" };
-//         } else if (e.id === id + 1) {
-//           return { ...e, state: "center" };
-//         } else {
-//           return { ...e, state: "side" };
-//         }
-//       })
-//     );
-//     console.log(list);
-//   }
-//   return (
-//     <div className="section h-[100vh] w-[100vw] pt-[8%] flex flex-col items-center  relative">
-//       <p className=" text-[#F05323] text-center text-[60px] font-[700] font-good-times mb-[50px] uppercase [text-shadow:0px_0px_17.8px_#F05323] z-[100] max-md:text-[40px] max-md:mt-[10%] ">
-//         FAQ
-//       </p>
-//       {list.map((e, i) => {
-//         return (
-//           <div
-//             key={i}
-//             className={` origin-right transition-all flex flex-col items-center absolute z-[10] top-[300px]   ${
-//               e.state === "center" ? "scale-100" : "scale-0"
-//             }`}
-//             onClick={() => handle(e.id < 5 ? e.id : 0)}
-//           >
-//             <div className="font-bold text-white text-[50px] mb-[30px]">
-//               {e.quest}
-//             </div>
-//             <div className="h-[300px] w-[31.5vw] min-w-[400px]  bg-[#262525]  rounded-[20px] flex justify-center items-center px-[50px]">
-//               <p className="text-[#FFF] font-[Poppins] leading-[34px] font-[500] text-[22px] max-md:text-[15px] leading-[30px]">
-//                 {" "}
-//                 {e.ans}
-//               </p>
-//             </div>
-//           </div>
-//         );
-//       })}{" "}
-//       *
-//     </div>
-//   );
-// };
-
-// export default Faq;
 
 import React from "react";
 import { Splide, SplideTrack, SplideSlide } from "@splidejs/react-splide";
 import "@splidejs/react-splide/css/core";
 
 import { BsFillTriangleFill } from "react-icons/bs";
-import SponsorCard from "./SponsorCard";
-import faqData from "./faqData";
+
+import faqData from "../data/faqData";
 import FaqqCard from "./FaqqCard";
 const Faq = () => {
   return (
